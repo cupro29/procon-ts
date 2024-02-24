e323788ca9bfe6b1ca11f5f8644e6247d0f15c4d
cupro31 <cupro.m454@gmail.com>
2024-02-25 03:18:53 +0900

    tabun iikanji

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,26 @@
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "airbnb-base",
-        "prettier"
-    ],
-    "root": true,
-    "env": {
-        "node": true,
-        "es2022": true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "airbnb-base",
+    "prettier",
+  ],
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+  },
+  parser: "@typescript-eslint/parser",
+  rules: {
+    "import/extensions": "off",
+    "no-console": "off",
+    "no-unused-vars": ["error", { args: "none" }],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
     },
-    "parser": "@typescript-eslint/parser",
-    "rules": {
-        "no-console": "off",
-    }
+  },
 };
